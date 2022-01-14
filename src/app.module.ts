35382eb888781b8f135212e4d3ec5cfbaeab5e49
Daniel Vitor <daniel.santos@brius.com.br>
2022-01-14 16:34:51 -0300

    Create a dto, interface and schema for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://teste777:Wb7aMjDuYDQScQx@cluster0.uvhnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
